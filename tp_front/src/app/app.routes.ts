9e84007c00f282a76d5666ac78bd9210e867d0e4
L4nd00 <leandro.marcial13@gmail.com>
2024-11-11 00:40:03 -0300

    componente navbar y funcionamiento de los links de este

--- tp_front/src/app/app.routes.ts
+++ tp_front/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'Calendario', component: CalendarioComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
     
 ];
