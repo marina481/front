b30cd5420594c75c5009df3d97d64ca42f8d013c
L4nd00 <leandro.marcial13@gmail.com>
2024-11-13 20:52:18 -0300

    Funcionamiento de disponibilidad de habitaciones

--- tp_front/src/app/calendario/calendario.component.ts
+++ tp_front/src/app/calendario/calendario.component.ts
@@ -1,28 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { Reserva } from '../models/reserva.model';
+import { ApiService } from '../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
-  imports: [NavbarComponent,FormsModule],
+  imports: [NavbarComponent,FormsModule,CommonModule],
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.css'
 })
-export class CalendarioComponent {
-  currentDate: string;
+export class CalendarioComponent implements OnInit{
+  currentDate: string = new Date().toISOString().split('T')[0];
   maxDate: string;
   minDate: string;
 
-  constructor() {
+  habitaciones: number[] = [101, 102, 103, 104, 105, 201, 202, 203, 204, 205, 301, 302, 303, 304,305];
+  listaReserva: Reserva[]= [];
+
+  constructor(private _backservice: ApiService) {
     // Establecer la fecha actual como la fecha predeterminada
     const today = new Date();
     this.currentDate = today.toISOString().split('T')[0]; // Solo la parte de la fecha (YYYY-MM-DD)
 
     // Configurar la fecha mínima y máxima
-    this.maxDate = today.toISOString().split('T')[0]; // Puedes cambiar este valor según lo que necesites
-    this.minDate = '2020-01-01'; // Establecer una fecha mínima arbitraria
+    this.maxDate = '2025-12-31'; // Puedes cambiar este valor según lo que necesites
+    this.minDate = this.currentDate; // Establecer una fecha mínima arbitraria
+
   }
+
+   // Al inicializar
+  ngOnInit(): void {
+    this._backservice.getReservas().subscribe((info:Reserva[]) => {
+     this.listaReserva = info;
+    })
+  }
+
+  //Comprueba si las habitaciones estan ocupadas en la fecha seleccionada
+  isRoomOccupied(roomNumber: number): boolean {
+    return this.listaReserva.some(reserva => {
+      const fechaInicio = new Date(reserva.FECHA_INICIO);
+      const fechaFin = new Date(reserva.FECHA_FIN);
+      const fechaSeleccionada = new Date(this.currentDate);
+  
+      return (
+        reserva.HABITACION === roomNumber &&
+        fechaSeleccionada >= fechaInicio &&
+        fechaSeleccionada <= fechaFin
+      );
+    });
+  }
+  
 
   // Función para retroceder o avanzar la fecha
   changeDate(direction: number): void {
---
+++ tp_front/src/app/models/reserva.model.ts
@@ -0,0 +1,9 @@
+export interface Reserva{
+    ID: number;
+    NOMBRE: string;
+    TELEFONO: number;
+    EMAIL: string;
+    HABITACION: number;
+    FECHA_INICIO: Date;
+    FECHA_FIN: Date;
+}
