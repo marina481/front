fdb287fd8b75adaf10bf454b12bedd73de84ba84
Antonella Carbone <antonella.a.carbone@gmail.com>
2024-11-17 19:27:10 -0300

    Habitaciones/ver/agregar

--- tp_front/src/app/app.routes.ts
+++ tp_front/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CalendarioComponent } from './calendario/calendario.component';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component';
 
 //Conexion de los path a los componentes
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'Calendario', component: CalendarioComponent },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: 'Habitaciones', component:HabitacionesComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+    
     
 ];
